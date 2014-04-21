d0234e4e5fe8631dad3ff9f7c9e4f0322f398a2a
spencereldred <eldredspencer@gmail.com>
2014-04-21 12:31:40 -0700

    Added comments to controller and view pages

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,36 +2,48 @@ class TodosController < ApplicationController
 
   def index
     # Retrieve a list of all the todo items from the database.
+    # Prepares the @todos instance variable for the index.html.erb page.
+    
     @todos = Todo.all
   end
 
   def new
-    #Make an empty Todo object for the form
+    #Make an empty Todo object (@todo) for the form on the new.html.erb page.
+    
     @todo = Todo.new
   end
 
   def create
-    # Take new page data (todo object) and save it to the database.
+    # Creates a todo item in the database with the information passed in
+    # the params hash. Redirects to the show page.
+    
     # binding.pry
-
     Todo.create todo_params
-    redirect_to action: :index
+    redirect_to @todo
   end
 
   def show
-    # Retrieve a single item from the database for the show page.
+    # Retrieves a specific item from the database. The id of the item is in
+    # the params hash.  Place the item in the @todo instance variable for 
+    # the show.html.erb page.
+
     # binding.pry
     @todo = Todo.find(params[:id])
   end
 
   def edit
-    # Retrieve a single item from the database for the edit page.
+    # Retrieves a specific item from the database. The id of the item is in
+    # the params hash. Place the item in the @todo instance variable 
+    # for the edit.html.erb page.
+
     # binding.pry
     @todo = Todo.find(params[:id])
   end
 
   def update
-    # Take edit page changes (todo object) and update the database.
+    # Retrieves a specific item from the database. The id of the item is in
+    # the params hash. Updates the database with the contents of the params hash.
+    # Redirects the view to the show page.
 
     # binding.pry
     @todo = Todo.find(params[:id])
@@ -40,9 +52,11 @@ class TodosController < ApplicationController
   end
 
   def destroy
-   # Delete a single record in the database.
+   # Delete a single record in the database. 
+   # Redirects the view to the index page.
+
    Todo.find(params[:id]).destroy
-    redirect_to action: :index
+   redirect_to action: :index
   end
 
   private
