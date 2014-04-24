0e16fd4f35a0e7d3a03b8a15e3748543bf9232e3
tigarcia <tim.m.garcia@gmail.com>
2014-04-24 09:35:34 -0700

    Getting the code ready for e4

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140418190630) do
+ActiveRecord::Schema.define(version: 20140424163426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140418190630) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "done",        default: false
   end
 
 end
